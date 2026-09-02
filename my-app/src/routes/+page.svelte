3614d60f5d2dfa53bb35873bf710f3dbb563e49f
MaartentePoele <maarten.te.poele@hva.nl>
2026-09-02 16:55:13 +0200

    oefenen met svelte link naar directus

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  let { data } = $props();
+
+  const persons = data.persons;
+</script>
+
+<h1>Home</h1>
+{#each persons as person}
+  <a href="/{person.id}">
+    <img src="https://fdnd.directus.app/assets/{person.mugshot}" alt="" width="100"/>
+  </a>
+{/each}
