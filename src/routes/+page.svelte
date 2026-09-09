8ba30eb3069ae4a3f6e656734e95cd91e821beb0
MaartentePoele <maarten.te.poele@hva.nl>
2026-09-09 11:55:50 +0200

    #3 Stylesheet toegevoegd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,10 +5,21 @@
     <h2>Frontend Developer</h2>
     <ul>
       <li>06-12345678</li>
-      <li>maarten.te.poele@hav.nl</li>
-      <li><a href="https://maartentepoele.github.io/i-love-web/">Portfolio</a></li>
-      <li><a href="https://www.linkedin.com/in/maarten-te-poele-3a7a57332/">LinkedIn</a></li>
-      <li><a href="https://github.com/MaartentePoele">GitHub</a></li>
+      <li>maarten.te.poele@hva.nl</li>
+      <li>
+        <a href="https://maartentepoele.github.io/i-love-web/" target="_blank"
+          >Portfolio</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://www.linkedin.com/in/maarten-te-poele-3a7a57332/"
+          target="_blank">LinkedIn</a
+        >
+      </li>
+      <li>
+        <a href="https://github.com/MaartentePoele" target="_blank">GitHub</a>
+      </li>
     </ul>
   </article>
 </main>
