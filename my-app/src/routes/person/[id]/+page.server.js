4305cfa418ecfe40f1e6665a4d252547598bf552
MaartentePoele <maarten.te.poele@hva.nl>
2026-09-09 11:02:57 +0200

    installed sveltekit

--- my-app/src/routes/person/[id]/+page.server.js
+++
@@ -1,6 +0,0 @@
-export async function load({ fetch, params }) {
-    const res = await fetch (`https://fdnd.directus.app/items/person/${params.id}`)
-    const data = await res.json()  
-
-    return { person: data.data }
-}
--- my-app/src/routes/person/[id]/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-  let { data } = $props();
-</script>
-
-<img src="https://fdnd.directus.app/assets/{data.person.mugshot}" alt="" width="300">
